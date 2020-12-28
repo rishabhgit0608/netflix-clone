cb1c438ad9c0190e58a1d96730aa629473b5e9ca
Rishabh Bhambani <bhambanirishabh@protonmail.com>
2020-12-29 01:53:17 +0530

    Added Rows component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
     <div className="App">
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}></Row>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}></Row>
+      <Row title="Top Rates" fetchUrl={requests.fetchTopRated}></Row>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}></Row>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}></Row>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}></Row>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}></Row>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}></Row>
     </div>
   );
 }
--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,7 +1,7 @@
 import React, { useState,useEffect } from 'react'
 import axios from '../axios'
-
-
+import '../Components/Row.css'
+const baseUrl = "https://image.tmdb.org/t/p/original/"
 
 function Row({title,fetchUrl}) {
     const [movies,setMovies] = useState([]);
@@ -9,19 +9,25 @@ function Row({title,fetchUrl}) {
     useEffect(()=>{
         async function fetchData(){
             const request = await axios.get(fetchUrl);
-            console.log(request);
+            setMovies(request.data.results);
+            return request;
         }
         fetchData();
-    },[]);
-
+    },[fetchUrl]);
     return (
-        <div>
+        <div className="row">
             {/* title */}
             <h2>{title}</h2>
-
+            <div className="row__posters">
             {/* container->posters */}
+            {movies.map(movie=>(
+            <img key="movie.id" className="row__poster"src={`${baseUrl}${movie.poster_path}`} alt ={movie.name}></img>
+            ))}
+
+            </div>
         </div>
     )
 }
+
 
 export default Row
