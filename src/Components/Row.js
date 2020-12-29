d251ff463953dff83da99424cdeaca57b321cf66
Rishabh Bhambani <bhambanirishabh@protonmail.com>
2020-12-30 04:56:38 +0530

    final commit netflix-clone project completed

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,34 +1,62 @@
-import React, { useState,useEffect } from 'react'
-import axios from '../axios'
-import '../Components/Row.css'
-const baseUrl = "https://image.tmdb.org/t/p/original/"
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
+import "../Components/Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
-function Row({title,fetchUrl,isLargeRow}) {
-    const [movies,setMovies] = useState([]);
-    //A snippet of code which runs based on a specific/variable
-    useEffect(()=>{
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    },[fetchUrl]);
-    console.log(movies);
-    return (
-        <div className="row">
-            <h2>{title}</h2>
-            <div className="row__posters">
-            {/* container->posters */}
-            {movies.map(movie=>(
-            <img
-             key="movie.id" className={`row__poster ${isLargeRow&&"row__posterLarge"}`}  src={`${baseUrl}${isLargeRow ? movie.poster_path:movie.backdrop_path}`} alt ={movie.name}>
-            </img>
-            ))}
-            </div>
-        </div>
-    )
+const baseUrl = "https://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState([]);
+  //A snippet of code which runs based on a specific/variable
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  const opts = {
+    height: "390px",
+    width: "100%",
+    playerVars: {
+      autoPlay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {/* container->posters */}
+        {movies.map((movie) => (
+          <img
+            key="movie.id"
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          ></img>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-
-export default Row
+export default Row;
